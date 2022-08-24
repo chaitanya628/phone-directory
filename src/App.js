8cbf26341169d75ddde2c7b1f3fbe06811f206cd
chaitanya628 <chaitanya.chandwani1699@gmail.com>
2022-08-24 13:40:45 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@
 
 function App() {
   return (
-    <div >
-      Phone Directory
-    </div>
+    <>
+      <div >
+        Phone Directory
+      </div>
+      <button>Add</button>
+      <div><span>Name</span><br />
+        <span>Phone</span>
+      </div>
+
+    </>
   );
 }
 

9d7af662a571ad5864ffc1b42fff4baaeaefde5b
chaitanya628 <chaitanya.chandwani1699@gmail.com>
2022-08-25 12:13:47 +0530

    Styled ‘Header’ (Inline Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 const Header = function()
 {
+    const headerstyle= {textAlign:'center',padding: 20, background:'black',color:'white',textTransform:'uppercase',fontWeight: 500,fontSize:'large'};
     return (
-        <div className="header">
+        <div style={headerstyle}>
             Phone Directory
         </div>
     )
